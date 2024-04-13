b965e4a13674722bfbd2f6224506ad664be3dfe6
shafikcmt <mdshafiqulislam822@gmail.com>
2024-04-13 19:14:11 +0530

    Added edit, delete button and design update page also add new config page

--- index.php
+++ index.php
@@ -8,36 +8,63 @@
                                 <th>Name</th>
                                 <th>Roll</th>
                                 <th>Course</th>
+                                <th>Action</th>
                             </tr>
                             <tr>
                                 <td>01</td>
                                 <td>Ram Kumar</td>
                                 <td>123456</td>
                                 <td>B.TECH</td>
+                                <td>
+                                    <a class="btn btn-primary" href="update.php">Edit</a>
+                                    <a class="btn btn-primary" href="#">Delete</a>
+
+                                </td>
+                               
                             </tr>
                             <tr>
                                 <td>02</td>
                                 <td>Ram Kumar</td>
                                 <td>123456</td>
                                 <td>B.TECH</td>
+                                <td>
+                                    <a class="btn btn-primary" href="update.php">Edit</a>
+                                    <a class="btn btn-primary" href="#">Delete</a>
+
+                                </td>
                             </tr>
                             <tr>
                                 <td>03</td>
                                 <td>Ram Kumar</td>
                                 <td>123456</td>
                                 <td>B.TECH</td>
+                                <td>
+                                    <a class="btn btn-primary" href="update.php">Edit</a>
+                                    <a class="btn btn-primary" href="#">Delete</a>
+
+                                </td>
                             </tr>
                             <tr>
                                 <td>04</td>
                                 <td>Ram Kumar</td>
                                 <td>123456</td>
                                 <td>B.TECH</td>
+                                <td>
+                                    <a class="btn btn-primary" href="update.php">Edit</a>
+                                    <a class="btn btn-primary" href="#">Delete</a>
+
+                                </td>
                             </tr>
                             <tr>
                                 <td>05</td>
                                 <td>Ram Kumar</td>
                                 <td>123456</td>
                                 <td>B.TECH</td>
+                                <td>
+                                    <a class="btn btn-primary" href="update.php">Edit</a>
+                                    <a class="btn btn-primary" href="#">Delete</a>
+
+                                </td>
                             </tr>
                       
                         </table>
--- update.php
+++ update.php
@@ -1,7 +1,27 @@
 <?php 
     include 'header.php';
  ?> 
- 
+ <div class="card-body">
+ <form action="/action_page.php">
+  <div class="mb-3 mt-3">
+    <label for="name" class="form-label">Name</label>
+    <input type="text" value="Shafiqul" class="form-control" id="name" placeholder="Enter Name" name="name">
+  </div>
+  <div class="mb-3">
+    <label for="roll" class="form-label">Roll:</label>
+    <input type="text" value="1234567" class="form-control" id="roll" placeholder="Enter Roll" name="roll">
+  </div>
+
+  <div class="mb-3">
+    <label for="pwd" class="form-label">Course:</label>
+    <input type="text" value="BTECH" class="form-control" id="course" placeholder="Enter Course" name="course">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Update</button>
+  <button type="submit" class="btn btn-primary">Delete</button>
+  <a class="btn btn-primary" href="index.php">Go Back</a>
+</form>
+ </div>
   <?php 
     include 'footer.php';
- ?>
+ ?> 
