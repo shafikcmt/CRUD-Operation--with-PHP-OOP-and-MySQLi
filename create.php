466df3ccc532eb9db0bfed84cbdd8d42d8dc963e
shafikcmt <mdshafiqulislam822@gmail.com>
2024-04-17 16:17:07 +0530

    Database connection and create page updated

--- create.php
+++ create.php
@@ -1,10 +1,17 @@
 <?php 
     include 'header.php';
+    $db = new Database();
+
+    if(isset($_POST['submit'])){
+      $name = $_POST['name'];
+      $name = $_POST['name'];
+      $name = $_POST['name'];
+    }
  ?> 
  <div class="card-body">
- <form action="/action_page.php">
+ <form action="" method="POST">
   <div class="mb-3 mt-3">
-    <label for="email" class="form-label">Name</label>
+    <label for="name" class="form-label">Name</label>
     <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
   </div>
   <div class="mb-3">
@@ -17,7 +24,7 @@
     <input type="text" class="form-control" id="course" placeholder="Enter Course" name="course">
   </div>
   
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
   <a class="btn btn-primary" href="index.php">Go Back</a>
 </form>
  </div>
